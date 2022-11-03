d31dddbb76f76c6637fa2f21d7ec36f2794270b6
BhushanDive <bhushandive43@gmail.com>
2022-11-03 22:11:56 +0530

    fixed the game frame rate at 60 fps.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ hello_button = pygame_gui.elements.UIButton(relative_rect = pygame.Rect((350, 27
 clock = pygame.time.Clock()
 
 while isRunning:
+    clock.tick(60)
     time_delta = clock.tick(60)/1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,7 +28,7 @@ while isRunning:
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == hello_button:
-                print('Hello World')
+                print('Lets Play')
         manager.process_events(event)
     manager.update(time_delta)
 
